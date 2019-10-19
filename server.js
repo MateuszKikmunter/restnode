a3f950c0210207166f3c93444ff103a1aee92b03
m.kikmunter <M.Kikmunter@gmail.com>
2019-10-19 12:20:43 +0100

    base server initialization

---
+++ server.js
@@ -0,0 +1,16 @@
+//import express and create server
+const express = require("express");
+const app = express();
+
+//server port
+const APP_PORT = 8080;
+
+//start server
+app.listen(APP_PORT, () => {
+    console.log(`server running and listening on ${APP_PORT}`);
+});
+
+//root endpoint
+app.get("/", (req, res, next) => {
+    res.json("{ message: OK }");
+});
